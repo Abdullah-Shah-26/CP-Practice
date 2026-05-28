0b7b1bdb453307742aa43f4f5ce145936e658816
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-05-28 17:40:34 +0530

    Update

---
+++ B_Odd_Grasshopper.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+// Ordered Set
+#define oset tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+// order_of_key(k) : No of Elements < k
+// *find_by_order(i) : Value at idx i (0 - based)
+
+// ---------- Type aliases ----------
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using vs = vector<string>;
+using vb = vector<bool>;
+using vvb = vector<vector<bool>>;
+using vpii = vector<pii>;
+using vvpii = vector<vector<pii>>;
+using vpll = vector<pll>;
+using vvpll = vector<vector<pll>>;
+
+// ---------- Constants ----------
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+// ---------- Fast IO ----------
+static const auto fastio = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+// ---------- Macros ----------
+#define rv(a) for(auto &x:(a)) cin>>x
+#define pv(a) do{ for(const auto &x:(a)) cout<<x<<' '; cout<<'\n'; }while(0)
+#define rm(mat) for(auto &r:(mat)) for(auto &x:(r)) cin>>x
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define pb push_back
+#define eb emplace_back
+#define rs resize
+#define as assign
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define yno(a) cout << ((a) ? "YES\n" : "NO\n")
+#define rep(i,a,b) for(int i=(a);i<(b);++i)
+#define per(i,a,b) for(int i=(b)-1;i>=(a);--i)
+#define endl '\n'
+
+// ---------- Solve ---------
+void solve(){
+  ll x0, n;
+  cin >> x0 >> n; // st, jumps
+
+  // Calculating final pos with respect to origin - 0
+  ll finalX;
+  
+  if(n % 4 == 1)
+    finalX = -n;
+  else if(n % 4 == 2)
+    finalX = 1;
+  else if(n % 4 == 3)
+    finalX = n + 1;
+  else if(n % 4 == 0)
+    finalX = 0;
+
+  if(x0 % 2 == 0)
+    finalX = x0 + finalX;
+  else 
+    finalX = x0 - finalX;
+
+  cout << finalX << endl;
+}
+
+// ---------- Main ----------
+int main(){
+  int t; 
+  cin >> t;
+  while(t--)
+    solve();
+  return 0;
+}
